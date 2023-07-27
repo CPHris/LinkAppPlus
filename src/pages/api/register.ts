4027370ad4943ad2012a51deba71b85f07973303
elena.sestrasound <elena.sestrasound@gmail.com>
2023-07-27 10:14:25 +0200

    created api route for register a new user

---
+++ src/pages/api/register.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { UserController } from './UserController';
+
+type Data = {
+  message: string;
+};
+
+export default function handler (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === 'POST') {
+    const user = req.body;
+    if (!user.email || !user.username) {
+      return res.status(400).send({ message: "Missing email/username" });
+    }
+    try {
+      UserController.addUser(user);
+      res.status(201).json({ message: 'User created' });
+    } catch (err) {
+      res.status(400).json({ message: (err as Error).message });
+    }
+  }
+}
