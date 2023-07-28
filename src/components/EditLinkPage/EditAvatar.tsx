5697eea5afffdd57c3e3a11f7dff8a25592ea723
elena.sestrasound <elena.sestrasound@gmail.com>
2023-07-28 20:05:26 +0200

    feat: being able to edit the name, avatar and bio fields in the ui

--- src/components/EditLinkPage/EditAvatar.tsx
+++ src/components/EditLinkPage/EditAvatar.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
+import { useState } from 'react';
 
 export interface IEditAvatarProps {
-  img?: string;
-  letters: string;
+  img?: string,
+  letters: string,
+  onChange: (value: string | undefined) => void;
 }
 
 export default function EditAvatar (props: IEditAvatarProps) {
+  const [img, setImg] = useState<string | undefined>(props.img);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImg(e.target.value);
+    props.onChange(img);
+  };
   return (
     <div className='w-full p-8 mb-5 bg-white rounded-lg'>
       <h2 className='block text-center mb-2 font-semibold'>Avatar</h2>
-      {props.img ? <img src={props.img} className='mt-0 mb-5 mx-auto w-36 rounded-full' /> :
+      {props.img || props.img !== '' ? <img src={props.img} className='mt-0 mb-5 mx-auto w-36 rounded-full' /> :
         <div>{props.letters.toUpperCase()}</div>
       }
-      <input type='text' value={props.img} name='avatarImg' className='block mx-auto my-0 px-3 bg-gray-200 rounded-md' />
+      <input onChange={onChangeHandler} type='text' value={img} name='avatarImg' className='block mx-auto my-0 px-3 bg-gray-200 rounded-md' />
     </div>
   );
 }
--- src/pages/user/[username]/edit/[linkpageid]/index.tsx
+++ src/pages/user/[username]/edit/[linkpageid]/index.tsx
@@ -3,7 +3,9 @@ import EditLink from '@/components/EditLinkPage/EditLink';
 import Layout from '@/components/Layout';
 import { RootState } from '@/store';
 import { LinkPage } from '@/types/User';
+import Link from 'next/link';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export interface IEditLinkPageProps {
@@ -12,25 +14,43 @@ export interface IEditLinkPageProps {
 export default function EditLinkPage () {
   const user = useSelector((state: RootState) => state.user);
   const linkPage = getLinkPage('page1', user.linkPages);
+
+  const [avatarImg, setAvatarImg] = useState<string | undefined>(linkPage.avatarImg);
+  const [name, setName] = useState(linkPage.name);
+  const [description, setDescription] = useState(linkPage.description);
+
+  const onNameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onDescriptionChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(e.target.value);
+  };
+
+  const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Change state and sumbit changes to backend");
+  };
+
   return (
     <Layout username={user.username} email={user.email}>
-      <main className='px-5 '>
-        <form className='container max-w-3xl mx-auto my-0'>
-          <EditAvatar img={linkPage.avatarImg} letters={user.username[0]} />
+      <main className='px-5'>
+        <form className='container max-w-3xl mx-auto my-0' onSubmit={submitForm}>
+          <EditAvatar img={linkPage.avatarImg} letters={user.username[0]} onChange={setAvatarImg} />
           <div className='w-full p-8 mb-5 bg-white rounded-lg'>
             <h2 className='block text-center mb-2 font-semibold'>Name</h2>
-            <input className='block mx-auto my-0 px-3 bg-gray-200 text-center rounded-md' type='text' value={linkPage.name} />
+            <input className='block mx-auto my-0 px-3 bg-gray-200 text-center rounded-md' type='text' value={name} onChange={onNameChangeHandler} />
           </div>
           <div className='w-full p-8 mb-5 bg-white rounded-lg'>
             <h2 className='block text-center mb-2 font-semibold'>Bio</h2>
-            <textarea className='block mx-auto my-0 py-3 px-5 bg-gray-200 w-96 h-32 text-left rounded-md' value={linkPage.description}></textarea>
+            <textarea onChange={onDescriptionChangeHandler} className='block mx-auto my-0 py-3 px-5 bg-gray-200 w-96 h-32 text-left rounded-md' value={description}></textarea>
           </div>
           {
             linkPage.links.map(link => <EditLink link={link} />)
           }
           <div className='flex justify-end'>
-            <button className='text-white bg-red-400 rounded-md p-3 mr-3 font-semibold'>Cancel</button>
-            <button className='text-white bg-green-400 rounded-md p-3 font-semibold'>Save Changes</button>
+            <Link href={`/user/${user.username}`} className='text-white bg-red-400 rounded-md p-3 mr-3 font-semibold'>Cancel</Link>
+            <button type="submit" className='text-white bg-green-400 rounded-md p-3 font-semibold' >Save Changes</button>
           </div>
         </form>
       </main>
