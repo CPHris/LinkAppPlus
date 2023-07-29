203de0a178128e1ebb9fff5b0fa222435bf60cb4
elena.sestrasound <elena.sestrasound@gmail.com>
2023-07-29 10:51:01 +0200

    chore: able to edit link images and urls

--- src/components/EditLinkPage/EditLink.tsx
+++ src/components/EditLinkPage/EditLink.tsx
@@ -14,6 +14,7 @@ export default function EditLink (props: IEditLinkProps) {
   const [title, setTitle] = useState(link.title);
   const [subtitle, setSubtitle] = useState(link.subtitle);
   const [img, setImg] = useState(link.img);
+  const [url, setUrl] = useState(link.url);
 
   const toggleEditingMode = () => {
     setEditingMode(prevState => !prevState);
@@ -25,7 +26,7 @@ export default function EditLink (props: IEditLinkProps) {
       img,
       title,
       subtitle,
-      url: link.url
+      url
     };
     props.onChange(editedLink, props.index);
     toggleEditingMode();
@@ -39,26 +40,44 @@ export default function EditLink (props: IEditLinkProps) {
     setSubtitle(e.target.value);
   };
 
-  return (
-    <div className='w-96 p-5 mb-5 bg-white rounded-lg flex flex-row items-center'>
-      <div className='h-full mr-2 flex flex-col'>
-        {isEditingModeEnabled
-          ? <button type='button' className='px-1 bg-green-400 rounded mb-7'><i className="fa-regular fa-circle-check fa-xs text-white" onClick={wrapLink}></i></button>
-          : <button type='button' className='px-1 border-solid border border-cyan-500 rounded mb-7'><i className="fa-solid fa-edit fa-xs text-cyan-500" onClick={toggleEditingMode}></i></button>
-        }
-        <button type='button' className='px-1 bg-red-400 rounded'><i className="fa-solid fa-trash fa-xs text-white"></i></button>
-      </div>
-      <img src={link.img} className='w-20 rounded-md flex mr-5' />
-      {isEditingModeEnabled
-        ? (<div className='mr-3'>
-          <input type='text' onChange={onTitleChange} value={title} className='block mx-auto mt-0 mb-3 px-3 bg-gray-200 text-center rounded-md' />
-          <input type='text' onChange={onSubtitleChange} value={subtitle} className='block mx-auto my-0 px-3 bg-gray-200 text-center rounded-md' />
-        </div>)
-        : (<div className='mr-3'>
-          <span className='block font-bold'>{title}</span>
-          <span className='block '>{subtitle}</span>
-        </div>)}
+  const onUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+  };
 
+  const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImg(e.target.value);
+  };
+
+  return (
+    <div className='w-96 p-5 mb-5 bg-white rounded-lg'>
+      <div className='flex flex-row items-center mb-5'>
+        <div className='h-full mr-5 flex flex-col'>
+          {isEditingModeEnabled
+            ? <button type='button' className='px-1 bg-green-400 rounded mb-7'><i className="fa-regular fa-circle-check fa-xs text-white" onClick={wrapLink}></i></button>
+            : <button type='button' className='px-1 border-solid border border-cyan-500 rounded mb-7'><i className="fa-solid fa-edit fa-xs text-cyan-500" onClick={toggleEditingMode}></i></button>
+          }
+          <button type='button' className='px-1 bg-red-400 rounded'><i className="fa-solid fa-trash fa-xs text-white"></i></button>
+        </div>
+        <img src={link.img} className='w-20 rounded-md flex mr-5' />
+        {isEditingModeEnabled
+          ? (<div className='mr-3'>
+            <input type='text' onChange={onTitleChange} value={title} className='block mx-auto mt-0 mb-3 px-3 bg-gray-200 text-center rounded-md' />
+            <input type='text' onChange={onSubtitleChange} value={subtitle} className='block mx-auto my-0 px-3 bg-gray-200 text-center rounded-md' />
+          </div>)
+          : (<div className='mr-3'>
+            <span className='block font-bold'>{title}</span>
+            <span className='block '>{subtitle}</span>
+          </div>)}
+
+      </div>
+      {isEditingModeEnabled && (
+        <div>
+          <label className='text-xs font-semibold'>Image URl</label>
+          <input type='text' value={img} onChange={onImageChange} className='block mb-3 px-3 bg-gray-200 rounded-md' />
+          <label className='text-xs font-semibold'>Link URL</label>
+          <input type='text' value={url} onChange={onUrlChange} className='block mb-3 px-3 bg-gray-200 rounded-md' />
+        </div>
+      )}
     </div>
   );
 }
