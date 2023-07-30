64ce3172873dc472ac7a11cd177115211a0e12ee
elena.sestrasound <elena.sestrasound@gmail.com>
2023-07-30 11:13:49 +0200

    style: basic styling for drompdown menu

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -15,9 +15,8 @@ export default function NavBar (props: INavBarProps) {
     setIsDropDownMenuVisible(prevState => !prevState);
   };
   return (
-    <div className='flex justify-between items-center px-4 py-2 mb-5 border-b border-gray-400'>
+    <div className='flex justify-between items-center px-4 py-2 mb-5 border-b border-gray-400 relative'>
       <div className='font-bold text-lg'>LinkApp</div>
-      {/* <div onClick={toggleDropDownMenu}>{username[0].toUpperCase()}</div> */}
       <button className='block w-fit px-4 py-2 w-fit rounded-full bg-cyan-500 text-white font-bold text-md ' onClick={toggleDropDownMenu}>{username[0].toUpperCase()}</button>
       {isDropDownMenuVisible && <DropDownMenu username={username} email={email} onclick={toggleDropDownMenu} />}
     </div>
@@ -31,14 +30,21 @@ interface IDropDownMenuProps {
 function DropDownMenu (props: IDropDownMenuProps) {
   const { username, email, onclick } = props;
   return (
-    <div>
-      <div>
-        <div>{username}</div>
-        <div>{email}</div>
+    <div className='p-4 border border-gray-400 rounded-lg absolute top-14 right-0 bg-white'>
+      <button type='button' onClick={onclick}>
+        <i className="fa-regular fa-circle-xmark text-gray-400"></i>
+      </button>
+      <div className='flex flex-col items-center px-4 mb-5'>
+        {/* <button className='block w-fit px-4 py-2 w-fit rounded-full bg-cyan-500 text-white font-bold text-md mb-2'>{username[0].toUpperCase()}</button> */}
+        <div className='font-semibold text-md'>{username}</div>
+        <div className='italic text-xs'>{email}</div>
       </div>
       <div>
-        <Link href={`/user/${username}/profile`} onClick={onclick}>Settings</Link>
-        <Link href='/login'>Logout</Link>
+        <Link className='block border-b border-gray-400 p-2'
+          href={`/user/${username}/profile`}
+          onClick={onclick}><i className="fa-solid fa-gear mr-2"></i>Settings </Link>
+        <Link className='block text-red-400 p-2'
+          href='/login'><i className="fa-solid fa-arrow-right-from-bracket mr-2"></i>Logout</Link>
       </div>
     </div>
   );
