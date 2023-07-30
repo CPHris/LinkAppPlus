3f9a6b943ba3bcb418a249eef0185ce6c7f0277b
elena.sestrasound <elena.sestrasound@gmail.com>
2023-07-30 11:48:12 +0200

    fix: fixed a visual bug where total visits where not in the center of the user page

--- src/components/UserPage/LinkPageListItem.tsx
+++ src/components/UserPage/LinkPageListItem.tsx
@@ -17,13 +17,13 @@ export default function LinkPagesListItem (props: ILinkPagesListItemProps) {
     props.deleteLinkPage(pageid);
   };
   return (
-    <div className='flex flex-row justify-between items-center border-b p-5'>
-      <span className='text-md font-semibold'>{name}</span>
-      <div>
-        <span className='block text-sm font-semibold'>Total visits</span>
+    <div className='flex felx-row basis-1/3 justify-between items-center border-b p-5'>
+      <span className='text-md font-semibold w-full'>{name}</span>
+      <div className='w-full'>
+        <span className='block text-sm font-semibold text-center'>Total visits</span>
         <span className='block text-sm text-center'>1790</span>
       </div>
-      <div>
+      <div className='w-full text-right'>
         <Link href={`${props.userRoute}/edit/${pageid}`} className='px-1 border-solid border border-cyan-500 rounded mb-7 mr-3'><i className="fa-solid fa-edit fa-xs text-cyan-500" ></i></Link>
         {/* <Link href={`${props.userRoute}/analytics/${pageid}`} className='px-1 border-solid border border-cyan-500 rounded mb-7'><i className="fa-sharp fa-regular fa-chart-line"></i></Link> */}
         <button type='button' className='px-1 bg-red-400 rounded' onClick={deleteLinkPage}><i className="fa-solid fa-trash fa-xs text-white"></i></button>
