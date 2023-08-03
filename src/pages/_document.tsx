4277763a71b0c45c23544c839cb24c7863fed0d7
Arto Lahtinen <artolle@pm.me>
2023-08-03 19:30:58 +0200

    fix: move app title to app.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>LinkAppPlus</title>
-      </Head>
+      <Head />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
