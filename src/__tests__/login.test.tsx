54a34f9a4455c4892bd680c38008bdee53c38fcd
Arto Lahtinen <artolle@pm.me>
2023-08-05 17:53:21 +0200

    Add first unit test with react-testing-library

---
+++ src/__tests__/login.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import RegisterPage from '@/pages/login/index';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+    };
+  },
+}));
+
+describe('Login', () => {
+  it('renders a heading', () => {
+    render(<RegisterPage />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Create an account/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders the login button', () => {
+    render(<RegisterPage />);
+
+    const loginButton = screen.getByRole('button', {
+      name: /login/i,
+    });
+
+    expect(loginButton).toBeInTheDocument();
+  });
+});
