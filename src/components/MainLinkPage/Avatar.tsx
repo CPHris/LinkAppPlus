ee2690ec3d58ae1c76aa073040385ece494d174b
Arto Lahtinen <artolle@pm.me>
2023-08-08 11:08:28 +0200

    fix: avatar image settings

--- src/components/MainLinkPage/Avatar.tsx
+++ src/components/MainLinkPage/Avatar.tsx
@@ -9,18 +9,20 @@ export interface IAvatarProps {
 export default function Avatar(props: IAvatarProps) {
   const { img, name } = props;
   return (
-    <div>
+    <>
       {img && img !== '' ? (
         <Image
           src={img}
           className="mt-0 mb-5 mx-auto w-36 rounded-full"
           alt="avatar"
+          height={800}
+          width={800}
         />
       ) : (
         <div className="px-10 py-8 w-fit rounded-full bg-cyan-500 text-white font-bold text-3xl mx-auto mb-5">
           {name[0] ? name[0].toUpperCase() : 0}
         </div>
       )}
-    </div>
+    </>
   );
 }
